21324c243c7a63218419f469098b75b760c25955
sayyamsingla <singlasayyam@gmail.com>
2026-06-05 13:15:58 -0700

    added simulate_game to game_simulator

--- app/simulation/game_simulator.py
+++ app/simulation/game_simulator.py
@@ -83,7 +83,41 @@ def simulate_possession(player, team, opponent, league_avg_def):
     
 
 def simulate_game(home_team, away_team, data):
-    pass
+    possessions = int((home_team['pace'] + away_team['pace']))
+    league_avg_def = get_league_avg_def_rating(data)
+
+    home_score = 0
+    away_score = 0 
+    home_team_players = home_team['roster']
+    home_team_weights = [p['usg_pct'] for p in home_team_players]
+    home_total = sum(home_team_weights)
+
+    away_team_players = away_team['roster']
+    away_team_weights = [p['usg_pct'] for p in away_team_players]
+    away_total = sum(away_team_weights)
+
+    for i in range(possessions):
+        if i % 2 == 0:
+            roll = random.random() * home_total
+            cumulative = 0
+            for player in home_team_players:
+                cumulative += player['usg_pct']
+                if roll < cumulative:
+                    selected_player = player
+                    break
+            home_score += simulate_possession(selected_player, home_team, away_team, league_avg_def)
+        else: 
+            roll = random.random() * away_total
+            cumulative = 0
+            for player in away_team_players:
+                cumulative += player['usg_pct']
+                if roll < cumulative:
+                    selected_player = player
+                    break
+            away_score += simulate_possession(selected_player, away_team, home_team, league_avg_def)
+    home_score = int(home_score * 1.01)
+    return home_score, away_score 
+    
 
 def simulate_playoffs(bracket, data):
     pass
@@ -99,93 +133,10 @@ if __name__ == '__main__':
     from data_fetcher import get_all_data
     
     data = get_all_data('2025-26')
-    league_avg_def = get_league_avg_def_rating(data)
     
-    # get OKC and their opponent BOS
     okc = data[1610612760]
     bos = data[1610612738]
     
-    # get Shai
-    shai = next(p for p in okc['roster'] if p['name'] == 'Shai Gilgeous-Alexander')
-    
-    # # simulate 10 possessions
-    # for i in range(100):
-    #     points = simulate_possession(shai, okc, bos, league_avg_def)
-    #     print(f"Possession {i+1}: {points} points")
-    total = sum(simulate_possession(shai, okc, bos, league_avg_def) for _ in range(100))
-    print(f"Total points in 100 possessions: {total}")
-    print(f"Points per possession: {total / 100}")
-
-# if __name__ == '__main__':
-#     import sys
-#     sys.path.append('../')
-#     from data_fetcher import get_all_data
-    
-#     data = get_all_data('2025-26')
-#     avg = get_league_avg_def_rating(data)
-#     print("League avg def rating:", avg)
-
-
-
-
-
-    # assumption
-# FOUL_SHOT_PENALTY_THREE = 0.05
-# FOUL_SHOT_PENALT_REGULAR = 0.25
-
-# def simulate_possession(player, team, opponent, league_avg_def):
-#     possessions_with_ball = 100 * (player['min_pg'] / 48) * player['usg_pct']
-#     tov_prob = player['tov_pg'] / possessions_with_ball
-#     if random.random() < tov_prob: 
-#         return 0
-#     defensive_modifier = opponent['def_rating']/ league_avg_def
-#     adjusted_fg_pct = player['fg_pct'] * defensive_modifier
-#     adjusted_fg3_pct = player['fg3_pct'] * defensive_modifier
-#     fouled_fg3_pct = adjusted_fg3_pct * FOUL_SHOT_PENALTY_THREE
-#     fouled_fg_pct = adjusted_fg_pct * FOUL_SHOT_PENALT_REGULAR
-#     foul_rate = player['fta_pg'] / (player['fga_pg'] * 2) if player['fga_pg'] > 0 else 0 
-
-
-#     three_point_rate = player['fg3a_pg'] / player['fga_pg'] if player['fga_pg'] > 0 else 0 
-#     is_three = random.random() < three_point_rate
-
-    
-#     if is_three:     
-#         if random.random() < foul_rate:
-#             if random.random() < fouled_fg3_pct: 
-#                if random.random() < player['ft_pct']:
-#                 return 4
-#                else: 
-#                 return 3
-#             else: 
-#                 total_count = 0
-#                 for i in range(3):
-#                     if random.random() < player['ft_pct']:
-#                         total_count += 1
-#                 return total_count
-#         else: 
-#             if random.random() < adjusted_fg3_pct: 
-#                return 3
-#             else: 
-#                return 0 
-#     else: 
-#         if random.random() < foul_rate: 
-#             if random.random() < fouled_fg_pct:
-#                 if random.random() < player['ft_pct']:
-#                     return 3
-#                 else: 
-#                     return 2
-#             else: 
-#                 total_count = 0
-#                 for i in range(2):
-#                     if random.random() < player['ft_pct']:
-#                         total_count += 1
-#                 return total_count
-#         else:
-#             if random.random() < adjusted_fg_pct: 
-#                return 2
-#             else: 
-#                return 0 
-
-# import random
-# from data_fetcher import get_all_data
+    home_score, away_score = simulate_game(okc, bos, data)
+    print(f"OKC: {home_score}")
+    print(f"BOS: {away_score}")
